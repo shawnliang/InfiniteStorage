2743841e49ba6b1ab86106c59af6581c96aaf74e
Shawn Liang <shawn.liang@waveface.com>
2013-04-02 17:06:31 +0800

    bonjour: register infinite storage with bonjour

--- Sources/InfiniteStorage/Program.cs
+++ Sources/InfiniteStorage/Program.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Bonjour;
 
 namespace InfiniteStorage
 {
 	static class Program
 	{
+		static BonjourService m_bonjourService;
+
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
@@ -15,7 +19,36 @@ namespace InfiniteStorage
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
+
+			try
+			{
+				m_bonjourService = new BonjourService();
+				m_bonjourService.Error += new EventHandler<BonjourErrorEventArgs>(m_bonjourService_Error);
+				m_bonjourService.Register(7777);
+			}
+			catch
+			{
+				MessageBox.Show("Bonjour service is not available", "Error");
+				return;
+			}
+
 			Application.Run(new Form1());
+
+			m_bonjourService.Unregister();
+		}
+
+		static void m_bonjourService_Error(object sender, BonjourErrorEventArgs e)
+		{
+			MessageBox.Show("Bonjour DNS operation error: " + e.error, "Error");
+		}
+
+		static void m_eventManager_ServiceRegistered(DNSSDService service, DNSSDFlags flags, string name, string regtype, string domain)
+		{
+		}
+
+		static void m_eventManager_OperationFailed(DNSSDService service, DNSSDError error)
+		{
+			MessageBox.Show("Operation returned an error code " + error, "Error");
 		}
 	}
 }
---
+++ Sources/InfiniteStorage/Src/Class/BonjourService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Bonjour;
+
+namespace InfiniteStorage
+{
+	class BonjourService
+	{
+		Bonjour.DNSSDService m_service;
+		Bonjour.DNSSDService m_register;
+		Bonjour.DNSSDEventManager m_eventManager;
+
+		public event EventHandler<BonjourErrorEventArgs> Error;
+
+		public BonjourService()
+		{
+			m_service = new DNSSDService();
+			m_eventManager = new DNSSDEventManager();
+			m_eventManager.ServiceRegistered += new _IDNSSDEvents_ServiceRegisteredEventHandler(m_eventManager_ServiceRegistered);
+			m_eventManager.OperationFailed += new _IDNSSDEvents_OperationFailedEventHandler(m_eventManager_OperationFailed);
+		}
+
+		void m_eventManager_OperationFailed(DNSSDService service, DNSSDError error)
+		{
+			var handler = Error;
+			if (handler != null)
+				handler(this, new BonjourErrorEventArgs { error = error });
+		}
+
+		void m_eventManager_ServiceRegistered(DNSSDService service, DNSSDFlags flags, string name, string regtype, string domain)
+		{
+		}
+
+		public void Register(ushort port)
+		{
+			var txt = new TXTRecord();
+			txt.SetValue("ws_port", Encoding.UTF8.GetBytes(port.ToString()));
+			txt.SetValue("version", Encoding.UTF8.GetBytes("1.0"));
+			m_register = m_service.Register(0, 0, Environment.UserName, "_infinite-storage._tcp", null, null, port, txt, m_eventManager);
+		}
+
+		public void Unregister()
+		{
+			if (m_register != null)
+				m_register.Stop();
+		}
+	}
+
+	class BonjourErrorEventArgs : EventArgs
+	{
+		public DNSSDError error { get; set; }
+	}
+}
