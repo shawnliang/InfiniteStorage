d28de1bc6e3c271a03c16de562a8142760c335d3
Michael Chen <michael.chen@waveface.com>
2013-05-02 16:46:40 +0800

    Refractory:change

--- src/com/waveface/sync/util/Log.java
+++ src/com/waveface/sync/util/Log.java
@@ -2,7 +2,7 @@ package com.waveface.sync.util;
 
 
 public class Log {
-	static final boolean LOG = false;
+	static final boolean LOG = true;
 
 	public static void i(String tag, String string) {
 		if (LOG)
