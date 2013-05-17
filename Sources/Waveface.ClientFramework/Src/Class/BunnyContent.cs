c0a40718fed00b7a67173f3bafaabd3104d1fd4a
larrynung <larrynung@hotmail.com>
2013-05-17 17:06:01 +0800

    add label function

---
+++ Sources/Waveface.ClientFramework/Src/Class/BunnyContent.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Waveface.Model;
+
+namespace Waveface.ClientFramework
+{
+	public class BunnyContent : Content
+	{
+
+		public override string ID
+		{
+			get
+			{
+				return GetContentID();
+			}
+		}
+
+		#region Constructor
+		public BunnyContent()
+		{
+
+		}
+
+		public BunnyContent(Uri uri)
+			: base(uri.GetHashCode().ToString(), uri)
+		{
+		} 
+		#endregion
+
+
+		#region Private Method
+		private string GetContentID()
+		{
+			//var resourceFolderValue = Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("BunnyHome").GetValue("ResourceFolder").ToString();
+			var savedPath = this.ContentPath;
+
+			var appDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Bunny");
+
+			var dbFilePath = Path.Combine(appDir, "database.s3db");
+
+			var conn = new SQLiteConnection(string.Format("Data source={0}", dbFilePath));
+
+			conn.Open();
+
+			var cmd = new SQLiteCommand(string.Format("SELECT * FROM Files where saved_path = '{0}'", savedPath), conn);
+
+			var contentID = cmd.ExecuteScalar().ToString();
+
+			conn.Close();
+
+			return contentID;
+		}
+		#endregion
+	}
+}
---
+++ Sources/Waveface.ClientFramework/Src/Class/StationAPI.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+namespace Waveface.ClientFramework
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Obfuscation]
+	public static class StationAPI
+	{
+		#region Const
+		private const string API_BASE_URL = @"http://127.0.0.1:14005";
+		private const string LABEL_API_BASE_URL = API_BASE_URL + @"/label";
+		#endregion
+
+
+		#region Private Static Method
+		/// <summary>
+		/// Toes the query string.
+		/// </summary>
+		/// <param name="nvc">The NVC.</param>
+		/// <returns></returns>
+		private static string ToQueryString(NameValueCollection nvc)
+		{
+			return string.Join("&", Array.ConvertAll(nvc.AllKeys, key => string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(nvc[key]))));
+		}
+
+		public static string Post(string uri, NameValueCollection parameters)
+		{
+			var queryString = ToQueryString(parameters);
+			var data = Encoding.Default.GetBytes(queryString);
+			var request = WebRequest.Create(uri) as HttpWebRequest;
+
+			request.Method = "POST";
+			request.ContentType = "application/x-www-form-urlencoded";
+			request.ContentLength = data.Length;
+
+			using (var requestStream = request.GetRequestStream())
+			{
+				requestStream.Write(data, 0, data.Length);
+			}
+
+			var response = request.GetResponse();
+			// Get the stream containing content returned by the server.
+			var dataStream = response.GetResponseStream();
+			// Open the stream using a StreamReader for easy access.
+			using (var sr = new StreamReader(dataStream))
+			{
+				return sr.ReadToEnd();
+			}
+		}
+		#endregion
+
+
+		#region Public Static Method
+		public static string GetAllLables()
+		{
+			var uri = LABEL_API_BASE_URL + "/list_all";
+
+			return Post(uri, new NameValueCollection(){});
+		}
+
+		public static string AddLabel(string labelID, string name)
+		{
+			var uri = LABEL_API_BASE_URL + "/add";
+
+			return Post(uri, new NameValueCollection() 
+			{
+				{"label_id", labelID},
+				{"name", name}
+			});
+		}
+
+		public static string Tag(string fileID, string labelID)
+		{
+			var uri = LABEL_API_BASE_URL + "/tag";
+
+			return Post(uri, new NameValueCollection() 
+			{
+				{"file_id", fileID},
+				{"label_id", labelID}
+			});
+		}
+
+		public static string UnTag(string fileID, string labelID)
+		{
+			var uri = LABEL_API_BASE_URL + "/untag";
+
+			return Post(uri, new NameValueCollection() 
+			{
+				{"file_id", fileID},
+				{"label_id", labelID}
+			});
+		}
+		#endregion
+	}
+}
