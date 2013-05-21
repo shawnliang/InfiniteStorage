d3e325bc2af8b4974c793ee08346a7e8c50fbd19
Shawn Liang <shawn.liang@waveface.com>
2013-05-21 15:25:19 +0800

    REST: implement label_cover api handler

--- Sources/InfiniteStorage/Program.cs
+++ Sources/InfiniteStorage/Program.cs
@@ -261,6 +261,7 @@ namespace InfiniteStorage
 			rest_server.AddHandler("/file/get", new FileGetApiHandler());
 			rest_server.AddHandler("/pending/get", new PendingGetApiHandler());
 			rest_server.AddHandler("/pending/sort", new PendingSortApiHandler());
+			rest_server.AddHandler("/label_cover", new LabelCoverApiHandler());
 			rest_server.Start();
 
 			return port;
---
+++ Sources/InfiniteStorage/Src/Class/REST/LabelCoverApiHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wammer.Station;
+using System.Net;
+
+namespace InfiniteStorage.REST
+{
+	class LabelCoverApiHandler : HttpHandler
+	{
+
+		public override void HandleRequest()
+		{
+			var args = Request.Url.AbsolutePath.Split(new string[] { @"/" }, StringSplitOptions.RemoveEmptyEntries);
+
+			if (args.Length != 2)
+				throw new FormatException("request url is not expected: " + Request.Url.AbsolutePath);
+
+			var label_id = new Guid(args[1]);
+
+			var util = new Notify.NotifySenderUtil();
+			var files = util.QueryLabeledFiles(label_id);
+
+			if (files.Any())
+				Response.RedirectLocation = new UriBuilder(Request.Url.Scheme, Request.Url.Host, Request.Url.Port, "/image/" + files.First().id.ToString() + "/medium").ToString();
+			else
+				Response.RedirectLocation = new UriBuilder(Request.Url.Scheme, Request.Url.Host, 12888, "/.resource/Empty.png").ToString();
+
+			Response.StatusCode = (int)HttpStatusCode.Moved;
+			Response.Close();
+		}
+	}
+}
