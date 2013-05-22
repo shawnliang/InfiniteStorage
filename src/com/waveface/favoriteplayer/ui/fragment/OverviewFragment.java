cc4e46d65f0b6f5b59da3ed005f02a186e617456
Jason Peng <jason1.peng@gmail.com>
2013-05-22 11:39:19 +0800

    Refactor: Implement basic label overview.

---
+++ src/com/waveface/favoriteplayer/ui/fragment/OverviewFragment.java
@@ -0,0 +1,99 @@
+package com.waveface.favoriteplayer.ui.fragment;
+
+import java.util.ArrayList;
+
+import com.waveface.favoriteplayer.Constant;
+import com.waveface.favoriteplayer.R;
+import com.waveface.favoriteplayer.db.LabelDB;
+import com.waveface.favoriteplayer.db.LabelFileTable;
+import com.waveface.favoriteplayer.entity.OverviewData;
+import com.waveface.favoriteplayer.entity.ServerEntity;
+import com.waveface.favoriteplayer.logic.ServersLogic;
+import com.waveface.favoriteplayer.ui.adapter.OverviewAdapter;
+import com.waveface.favoriteplayer.ui.widget.TwoWayView;
+
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OverviewFragment extends Fragment{
+	private TwoWayView mList;
+	private int mType = OVERVIEW_VIEW_TYPE_LABEL;
+	
+	public static final int OVERVIEW_VIEW_TYPE_LABEL = 1;
+
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.fragment_overview, container, false);
+		mList = (TwoWayView) root.findViewById(R.id.list);
+		
+		Bundle data = null;
+		if(savedInstanceState == null) {
+			data = getArguments();
+		} else {
+			data = savedInstanceState;
+		}
+		
+		if(data != null) {
+			mType = data.getInt(Constant.ARGUMENT1);
+		}
+		
+		new PrepareViewTask().execute(null, null, null);
+		
+		return root;
+	}
+	
+	private class PrepareViewTask extends AsyncTask<Void, Void, OverviewData[]> {
+
+		@Override
+		protected OverviewData[] doInBackground(Void... params) {
+
+			if(getActivity() == null)
+				return null;
+			OverviewData datas[] = null;
+			ArrayList<ServerEntity> servers = ServersLogic.getBackupedServers(getActivity());
+			ServerEntity pairedServer = servers.get(0);
+			String serverUrl ="http://"+pairedServer.ip+":"+pairedServer.restPort;
+			
+			switch(mType) {
+			case OVERVIEW_VIEW_TYPE_LABEL:
+				String labelId = null;
+				Cursor c = LabelDB.getAllLabels(getActivity());
+				if(c.getCount() > 0) {
+					c.moveToFirst();
+					labelId = c.getString(0);
+				}
+				c.close();
+				c = LabelDB.getLabelFilesByLabelId(getActivity(), labelId);
+				
+				datas = new OverviewData[c.getCount()];
+				for(int i=0; i<c.getCount(); ++i) {
+					c.moveToPosition(i);
+					OverviewData data = new OverviewData();
+					data.url = serverUrl + Constant.URL_IMAGE + "/" + 
+							c.getString(c.getColumnIndex(LabelFileTable.COLUMN_FILE_ID)) + 
+							Constant.URL_IMAGE_MEDIUM;
+					datas[i] = data;
+				}
+				c.close();
+				
+				break;
+			}
+			return datas;
+		}
+		
+		@Override
+		protected void onPostExecute(OverviewData[] datas) {
+			if(datas != null) {
+				OverviewAdapter adapter = new OverviewAdapter(getActivity(), datas);
+				mList.setAdapter(adapter);
+			}
+		}
+	}
+}
