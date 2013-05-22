f919c657344fc525d75a7378c4a5c1ecc7d2ec0d
kimichiu <chiuyinche@gmail.com>
2013-05-22 10:34:04 +0800

    clear code

--- src/com/waveface/favoriteplayer/logic/ServersLogic.java
+++ src/com/waveface/favoriteplayer/logic/ServersLogic.java
@@ -509,20 +509,41 @@ public class ServersLogic {
 				//TODO:CHANGE TO NEW PROTOCAL
 
 
-				context.sendBroadcast(new Intent(Constant.ACTION_WEB_SOCKET_SERVER_CONNECTED));
+				
+				Log.d(TAG, "onCreateView");
 				EventBus.getDefault().post(new WebSocketEvent(WebSocketEvent.STATUS_CONNECT));
-				//ConnectEntity connect = new ConnectEntity();
-				ConnectForGTVEntity connectForGTV = new ConnectForGTVEntity();
-				ConnectForGTVEntity.Connect  connect = new ConnectForGTVEntity.Connect();
-				connect.deviceId=DeviceUtil.id(context);
-				connect.deviceName = DeviceUtil
-						.getDeviceNameForDisplay(context);
-				connectForGTV.setConnect(connect);
-				ConnectForGTVEntity.Subscribe subscribe = new ConnectForGTVEntity.Subscribe();
-				subscribe.labels=false;
-				connectForGTV.setSubscribe(subscribe);
-				Log.d(TAG, "send message="+RuntimeState.GSON.toJson(connectForGTV));
-				RuntimeWebClient.send(RuntimeState.GSON.toJson(connectForGTV));				
+				Cursor cursor = LabelDB.getAllLabels(context);
+				String labelId = null;
+				String labSeq =null;
+				if(cursor!=null && cursor.getCount()>0){
+					cursor.moveToFirst();
+					labelId = cursor.getString(cursor.getColumnIndex(LabelTable.COLUMN_LABEL_ID));
+					labSeq=cursor.getString(cursor.getColumnIndex(LabelTable.COLUMN_SEQ));
+				
+				if(cursor.getCount()>0){
+						
+						ConnectForGTVEntity connectForGTV = new ConnectForGTVEntity();
+						ConnectForGTVEntity.Connect  connect = new ConnectForGTVEntity.Connect();
+						connect.deviceId=DeviceUtil.id(context);
+						connect.deviceName = DeviceUtil
+								.getDeviceNameForDisplay(context);
+						connectForGTV.setConnect(connect);
+						ConnectForGTVEntity.Subscribe subscribe = new ConnectForGTVEntity.Subscribe();
+						subscribe.labels=true;
+						subscribe.labels_from_seq=labSeq;
+						connectForGTV.setSubscribe(subscribe);
+						Log.d(TAG, "send message="+RuntimeState.GSON.toJson(connectForGTV));
+						RuntimeWebClient.send(RuntimeState.GSON.toJson(connectForGTV));
+						//send broadcast label change
+						Intent intent = new Intent(Constant.ACTION_LABEL_CHANGE);
+						context.sendBroadcast(intent);
+					}
+				}else{
+					//label is no data ,get data from bunny
+					Intent intent = new Intent(Constant.ACTION_WEB_SOCKET_SERVER_CONNECTED);
+					context.sendBroadcast(intent);
+
+				}	
 
 			} catch (WebSocketException e) {
 				e.printStackTrace();
