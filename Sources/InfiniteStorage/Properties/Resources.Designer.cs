24fdb9f66dc02acdb79b630c3f158162d9288cb4
Shawn Liang <shawn.liang@waveface.com>
2013-05-25 11:00:30 +0800

    use Favorit's program icon

--- Sources/InfiniteStorage/Properties/Resources.Designer.cs
+++ Sources/InfiniteStorage/Properties/Resources.Designer.cs
@@ -191,16 +191,6 @@ namespace InfiniteStorage.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap FAV_096 {
-            get {
-                object obj = ResourceManager.GetObject("FAV_096", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to 檔案：{0}   {1} bytes.
         /// </summary>
         internal static string FirstUse_FileLabel {
