b575e10d8ec663659aa3d2501be43faff3c8a9f7
Jason Peng <jason1.peng@gmail.com>
2013-05-28 16:46:30 +0800

    Refactor: Add label change event.

---
+++ src/com/waveface/favoriteplayer/event/LabelChangeEvent.java
@@ -0,0 +1,9 @@
+package com.waveface.favoriteplayer.event;
+
+public class LabelChangeEvent {
+	public String labelId;
+	
+	public LabelChangeEvent(String labelId) {
+		this.labelId = labelId;
+	}
+}
