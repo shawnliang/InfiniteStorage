4fc03cee039285449681ebf4e3c6d9880b641221
Jason Peng <jason1.peng@gmail.com>
2013-05-29 23:29:35 +0800

    Refactor: Avoid open tab activity twice.

--- src/com/waveface/favoriteplayer/ui/MainActivity.java
+++ src/com/waveface/favoriteplayer/ui/MainActivity.java
@@ -3,22 +3,17 @@ package com.waveface.favoriteplayer.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.text.TextUtils;
 import android.util.Log;
 
 import com.waveface.favoriteplayer.Constant;
 import com.waveface.favoriteplayer.R;
 import com.waveface.favoriteplayer.event.LabelImportedEvent;
 import com.waveface.favoriteplayer.event.ServerChooseEvent;
-import com.waveface.favoriteplayer.event.WebSocketEvent;
 import com.waveface.favoriteplayer.logic.ServersLogic;
 import com.waveface.favoriteplayer.task.DownloadLabelsTask;
-import com.waveface.favoriteplayer.ui.fragment.FragmentBase;
 import com.waveface.favoriteplayer.ui.fragment.PickserverDialogFragment;
-import com.waveface.favoriteplayer.ui.fragment.SyncFragment;
 import com.waveface.favoriteplayer.ui.fragment.SyncFragmentBase.onSyncFragmentChangedListener;
 import com.waveface.favoriteplayer.ui.fragment.SyncInProgressFragment;
 
@@ -32,8 +27,7 @@ import de.greenrobot.event.EventBus;
  */
 public class MainActivity extends FragmentActivity implements onSyncFragmentChangedListener{
 	private String TAG = MainActivity.class.getSimpleName();
-	private String mCurrentFragmentName = null;
-//    private DNSThread dnsThread = null;
+	private boolean mActivityLaunch = false;
 	
 	private Handler mHander = new Handler();
 	private Runnable mShowPlaybackRunnable = new Runnable() {
@@ -42,31 +36,13 @@ public class MainActivity extends FragmentActivity implements onSyncFragmentChan
 		public void run() {
 			startActivity(new Intent(MainActivity.this, MainTabActivity.class));
 			finish();
-//			if(OverviewFragment.class.getSimpleName().equals(mCurrentFragmentName) == false) {
-//				FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//				OverviewFragment overview = new OverviewFragment();
-//				transaction.replace(R.id.container_content, overview, OverviewFragment.class.getSimpleName()).commit();
-//				mCurrentFragmentName = OverviewFragment.class.getSimpleName();
-//			}
-//			VideoFragment videos = new VideoFragment();
-//			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//			transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-//			transaction.replace(R.id.container_content, videos, VideoFragment.class.getSimpleName()).commit();
-//			mCurrentFragmentName = VideoFragment.class.getSimpleName();
-			
-//			PlaybackFragment photoJournal = new PlaybackFragment();
-//			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//			transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-//			transaction.replace(R.id.container_content, photoJournal, PlaybackFragment.class.getSimpleName()).commit();
-//			mCurrentFragmentName = PlaybackFragment.class.getSimpleName();
 		}
 	};
 	
 	private Runnable mReplaceContentToSyncRunnable = new Runnable() {
 		@Override
 		public void run() {
-			showSyncInProgressFragment(false);
-			//new DownloadLabelsTask(MainActivity.this).execute(new Void[]{});
+			showSyncInProgressFragment();
 		}
 		
 	};
@@ -79,18 +55,12 @@ public class MainActivity extends FragmentActivity implements onSyncFragmentChan
 		setContentView(R.layout.activity_main);
 		
 		if(savedInstanceState == null) {
-			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-			transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
 			
 			if(ServersLogic.hasBackupedServers(this) == false) {
-//				SyncFragment photoJournal = new SyncFragment();
-//				transaction.add(R.id.container_content, photoJournal, SyncFragment.class.getSimpleName()).commit();
-//				mCurrentFragmentName = SyncFragment.class.getSimpleName();
-				
 				PickserverDialogFragment df = new PickserverDialogFragment();
 				df.show(getSupportFragmentManager(), PickserverDialogFragment.class.getSimpleName());
 			} else {			
-				showSyncInProgressFragment(false);
+				showSyncInProgressFragment();
 				new DownloadLabelsTask(this).execute(new Void[]{});
 			}
 		}
@@ -98,31 +68,17 @@ public class MainActivity extends FragmentActivity implements onSyncFragmentChan
 	}
 	
 	public void onEvent(LabelImportedEvent event) {
-		if(event.status == LabelImportedEvent.STATUS_DONE) {
+		if(event.status == LabelImportedEvent.STATUS_DONE && mActivityLaunch == false) {
+			mActivityLaunch = true;
 			mHander.post(mShowPlaybackRunnable);
 		}
 	}
 	
-	private void showSyncInProgressFragment(boolean replace) {
+	private void showSyncInProgressFragment() {
 		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 		transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
 		SyncInProgressFragment photoJournal = new SyncInProgressFragment();
-		if(replace) {
-			transaction.replace(R.id.container_content, photoJournal, SyncInProgressFragment.class.getSimpleName()).commit();
-		} else {
-			transaction.add(R.id.container_content, photoJournal, SyncInProgressFragment.class.getSimpleName()).commit();
-		}
-		mCurrentFragmentName = SyncInProgressFragment.class.getSimpleName();
-	}
-	
-	public void onEvent(WebSocketEvent event) {
-		if(event.status == WebSocketEvent.STATUS_CONNECT) {
-			if(SyncFragment.class.getSimpleName().equals(mCurrentFragmentName)) {
-				// since sync fragment is opening first use activity, can't do anything
-			} else if(SyncInProgressFragment.class.getSimpleName().equals(mCurrentFragmentName)){
-				//new DownloadLabelsTask(this).execute(new Void[]{});
-			}
-		}
+		transaction.add(R.id.container_content, photoJournal, SyncInProgressFragment.class.getSimpleName()).commit();
 	}
 	
 	public void onEvent(ServerChooseEvent event) {
@@ -152,9 +108,6 @@ public class MainActivity extends FragmentActivity implements onSyncFragmentChan
 
 	@Override
 	public void done(String id) {
-		if(SyncFragment.class.getSimpleName().equals(mCurrentFragmentName)) {
-			mHander.post(mReplaceContentToSyncRunnable);
-		}
 	}
 
 	@Override
