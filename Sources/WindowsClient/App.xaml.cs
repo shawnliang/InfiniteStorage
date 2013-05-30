9eba18fc5cffe8f9e7c78dc5d890785628c3bdb6
larrynung <larrynung@hotmail.com>
2013-05-30 17:32:12 +0800

    Use StartUpUri to assign startup page

--- Sources/WindowsClient/App.xaml.cs
+++ Sources/WindowsClient/App.xaml.cs
@@ -7,10 +7,5 @@ namespace Waveface.Client
 	/// </summary>
 	public partial class App : Application
 	{
-		void OnApplicationStartup(object sender, StartupEventArgs args)
-		{
-			MainWindow mainWindow = new MainWindow();
-			mainWindow.Show();
-		}
 	}
 }
