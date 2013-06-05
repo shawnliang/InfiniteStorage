32a4448d7b772aaf76fadda00c03186dc357b0da
Shawn Liang <shawn.liang@waveface.com>
2013-06-05 10:44:42 +0800

    REST: untag multi files

--- Sources/InfiniteStorage/Src/Class/REST/LabelUntagApiHandler.cs
+++ Sources/InfiniteStorage/Src/Class/REST/LabelUntagApiHandler.cs
@@ -11,29 +11,32 @@ namespace InfiniteStorage.REST
 		{
 			CheckParameter("file_id", "label_id");
 
-			var file_id = new Guid(Parameters["file_id"]);
+			var file_ids = Parameters["file_id"].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => new Guid(x)).ToList();
 			var label_id = new Guid(Parameters["label_id"]);
 
 			using (var db = new MyDbContext())
 			{
-				var labeledFile = (from lb in db.Object.LabelFiles
-								   where lb.label_id == label_id && lb.file_id == file_id
-								   select lb).FirstOrDefault();
+				var label = (from lb in db.Object.Labels
+							 where lb.label_id == label_id
+							 select lb).FirstOrDefault();
 
-				if (labeledFile != null)
+				if (label == null)
+					throw new ArgumentException("no such label_id: " + label_id.ToString());
+
+				foreach (var file_id in file_ids)
 				{
-					db.Object.LabelFiles.Remove(labeledFile);
+					var labeledFile = (from lb in db.Object.LabelFiles
+									   where lb.label_id == label_id && lb.file_id == file_id
+									   select lb).FirstOrDefault();
 
-					var label = (from lb in db.Object.Labels
-								 where lb.label_id == label_id
-								 select lb).FirstOrDefault();
-
-					if (label != null)
+					if (labeledFile != null)
+					{
+						db.Object.LabelFiles.Remove(labeledFile);
 						label.seq = SeqNum.GetNextSeq();
-
-					db.Object.SaveChanges();
+					}
 				}
 
+				db.Object.SaveChanges();
 			}
 
 			respondSuccess();
