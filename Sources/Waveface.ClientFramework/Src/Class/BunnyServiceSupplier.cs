c39ffc3f8fa7427a68a871ac9ab7f28f595ec5e1
Shawn Liang <shawn.liang@waveface.com>
2013-06-09 16:30:12 +0800

    recving: ui subscribes recving device notification from server

--- Sources/Waveface.ClientFramework/Src/Class/BunnyServiceSupplier.cs
+++ Sources/Waveface.ClientFramework/Src/Class/BunnyServiceSupplier.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
+using System.Linq;
 using Waveface.Model;
+using WebSocketSharp;
+using Newtonsoft.Json;
 
 namespace Waveface.ClientFramework
 {
@@ -48,6 +51,11 @@ namespace Waveface.ClientFramework
 		#endregion
 
 
+		#region Private Var
+		WebSocket ws;
+		private bool ws_inited;
+		#endregion
+
 		#region Private Method
 		private void SetServices()
 		{
@@ -74,7 +82,85 @@ namespace Waveface.ClientFramework
 			conn.Close();
 
 			this.Services = services;
+
+			if (!ws_inited)
+			{
+				subscribeActiveDeviceAsync();
+				ws_inited = true;
+			}
+		}
+
+		private void subscribeActiveDeviceAsync()
+		{
+			System.Threading.Tasks.Task t = new System.Threading.Tasks.Task(() => {
+				ws = new WebSocket("ws://127.0.0.1:13995/", new string[] { });
+				ws.OnOpen += new EventHandler(ws_OnOpen);
+				ws.OnClose += new EventHandler<CloseEventArgs>(ws_OnClose);
+				ws.OnMessage += new EventHandler<MessageEventArgs>(ws_OnMessage);
+
+
+				System.Threading.Thread.Sleep(1000);
+				ws.Connect();
+			});
+			t.Start();
+		}
+
+		void ws_OnOpen(object sender, EventArgs e)
+		{
+			var msg = new
+			{
+				connect = new
+				{
+					device_id = Guid.NewGuid().ToString(),
+					device_name = "MainUI"
+				},
+				subscribe = new {
+					devices = true
+				}
+			};
+
+			ws.Send(JsonConvert.SerializeObject(msg));
+		}
+
+		void ws_OnMessage(object sender, MessageEventArgs e)
+		{
+			if (e.Type != WebSocketSharp.Frame.Opcode.TEXT)
+				return;
+
+			try{
+				var notify = JsonConvert.DeserializeObject<NotifyMsg>(e.Data);
+				var active_devices = notify.active_devices;
+
+				var services = this.Services;
+
+				var actives = from s in services
+							  where active_devices.Contains(s.ID)
+							  select s;
+
+				foreach (BunnyService s in actives)
+					s.IsRecving = true;
+
+				var inactives = services.Except(actives);
+
+				foreach (BunnyService s in inactives)
+					s.IsRecving = false;
+			}
+			catch(Exception err)
+			{
+			
+			}
+		}
+
+		void ws_OnClose(object sender, CloseEventArgs e)
+		{
+			subscribeActiveDeviceAsync();
 		}
 		#endregion
+	}
+
+
+	internal class NotifyMsg
+	{
+		public List<string> active_devices { get; set; }
 	}
 }
