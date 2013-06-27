1683856917b13f1d760a2e73636eda889e1ca8f1
Shawn Liang <shawn.liang@waveface.com>
2013-06-27 15:58:09 +0800

    share: send invitation by pio_posts/send_favorite_email

--- Sources/InfiniteStorage/Src/Class/REST/LabelInviteApiHandler.cs
+++ Sources/InfiniteStorage/Src/Class/REST/LabelInviteApiHandler.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Data.SQLite;
 using InfiniteStorage.Model;
+using InfiniteStorage.Properties;
 
 namespace InfiniteStorage.REST
 {
@@ -21,60 +22,46 @@ namespace InfiniteStorage.REST
 			var sender = Parameters["sender"];
 			var msg = Parameters["msg"];
 
-			using (var conn = new SQLiteConnection(MyDbContext.ConnectionString))
-			{
-				conn.Open();
+			if (recipients == null || !recipients.Any())
+				throw new Exception("no recipients");
 
-				using (var transaction = conn.BeginTransaction())
-				{
-					var cmdQuery = conn.CreateCommand();
-					cmdQuery.CommandText = "select 1 from [LabelShareTo] where label_id = @label and email = @email";
-					cmdQuery.Prepare();
+			var label = getLabel(label_id);
 
-					var cmdInsert = conn.CreateCommand();
-					cmdInsert.CommandText = "insert into [LabelShareTo] (label_id, email, name) values (@label, @email, @name)";
-					cmdInsert.Prepare();
-
-					bool labelChanged = false;
-
-					foreach (var recipient in recipients)
-					{
-						cmdQuery.Parameters.Clear();
-						cmdQuery.Parameters.Add(new SQLiteParameter("@label", label_id));
-						cmdQuery.Parameters.Add(new SQLiteParameter("@email", recipient.email));
-
-						var recipientExists = cmdQuery.ExecuteScalar() != null;
-
-						if (!recipientExists)
-						{
-							cmdInsert.Parameters.Clear();
-							cmdInsert.Parameters.Add(new SQLiteParameter("@label", label_id));
-							cmdInsert.Parameters.Add(new SQLiteParameter("@email", recipient.email));
-							cmdInsert.Parameters.Add(new SQLiteParameter("@name", recipient.name));
-							cmdInsert.ExecuteNonQuery();
-							labelChanged = true;
-						}
-					}
-
-
-					if (labelChanged)
-					{
-						var cmd = conn.CreateCommand();
-						cmd.CommandText = "update labels set seq = @seq where label_id = @label";
-						cmd.Parameters.Add(new SQLiteParameter("@seq", (object)SeqNum.GetNextSeq()));
-						cmd.Parameters.Add(new SQLiteParameter("@label", label_id));
-						var affectedRows = cmd.ExecuteNonQuery();
-
-						if (affectedRows == 0)
-							throw new Exception("label_id does not exist: " + label_id.ToString());
-					}
-
-					transaction.Commit();
-				}
-			}
-
+			callCloudApiToSendEmail(label, sender, msg, recipients);
 
 			respondSuccess();
+		}
+
+		private void callCloudApiToSendEmail(Label label, string sender, string msg, ICollection<Recipient> recipients)
+		{
+			if (string.IsNullOrEmpty(label.share_code))
+				throw new Exception("label is not shared yet: " + label.label_id + " " + label.name);
+
+			postServiceLibrary.postServiceClass.sendFavoriteEmail(
+				Cloud.CloudService.SessionToken,
+				Cloud.CloudService.APIKey,
+				recipients.Select(x=>x.email).ToList(),
+				label.share_code,
+				sender,
+				label.name,
+				msg);
+		}
+
+		private static Model.Label getLabel(Guid label_id)
+		{
+			using (var db = new MyDbContext())
+			{
+				var q = from lb in db.Object.Labels
+						where lb.label_id == label_id
+						select lb;
+
+				var label = q.FirstOrDefault();
+
+				if (label == null || label.deleted == true)
+					throw new Exception("label does not exist or is already deleted: " + label_id);
+
+				return label;
+			}
 		}
 
 		private List<Recipient> getRecipients()
