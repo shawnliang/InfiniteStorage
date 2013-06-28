7698ffdbf1c3bd3e4ef014bb0dcf5378b93b4f64
larrynung <larrynung@hotmail.com>
2013-06-28 17:08:26 +0800

    adjust left side panel and apply l18n

--- Sources/WindowsClient/App.xaml.cs
+++ Sources/WindowsClient/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.Windows;
 
 namespace Waveface.Client
 {
@@ -7,5 +9,22 @@ namespace Waveface.Client
 	/// </summary>
 	public partial class App : Application
 	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			CultureInfo currentCultureInfo = CultureInfo.CurrentCulture;
+			try
+			{
+				var rd = Application.LoadComponent(new Uri(@"StringTable." + currentCultureInfo.Name + ".xaml ", UriKind.Relative)) as ResourceDictionary;
+
+				if (rd != null)
+				{
+					this.Resources.MergedDictionaries.Clear();
+					this.Resources.MergedDictionaries.Add(rd);
+				}
+			}
+			catch
+			{
+			}
+		}
 	}
 }
