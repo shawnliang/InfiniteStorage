775a929e91e06d7616a2cd4c6b1762cf20c4ddb7
Shawn Liang <shawn.liang@waveface.com>
2013-07-03 17:02:13 +0800

    REST: add manuipulation/delete api

--- Sources/InfiniteStorage/Src/Class/REST/ManipulationDeleteApiHAndler.cs
+++ Sources/InfiniteStorage/Src/Class/REST/ManipulationDeleteApiHAndler.cs
@@ -9,16 +9,16 @@ using System.Data.SQLite;
 
 namespace InfiniteStorage.REST
 {
-	class ManipulationDeleteApiHAndler : HttpHandler
+	class ManipulationDeleteApiHAndler : ManipulationApiHandlerBase
 	{
 		public override void HandleRequest()
 		{
-			var file_ids = parseToList(Parameters["ids"]).Select(x => new Guid(x)).ToList();
-			var folders = parseToList(Parameters["paths"]).Select(x => PathUtil.MakeRelative(x, MyFileFolder.Photo)).ToList();
+			var file_ids = GetIds();
+			var folders = GetPaths();
 
 
-			var filesToDelete = getFiles(file_ids);
-			filesToDelete.AddRange(getFilesFromFolders(folders));
+			var filesToDelete = GetFilesById(file_ids);
+			filesToDelete.AddRange(GetFilesFromFolder(folders));
 
 			List<AbstractFileToManipulate> notDeletedFiles;
 			var pendingDeleteFiles = moveFilesToRecycleBin(filesToDelete, out notDeletedFiles);
@@ -113,21 +113,7 @@ namespace InfiniteStorage.REST
 			return deleted;
 		}
 
-		private List<AbstractFileToManipulate> getFilesFromFolders(List<string> folders)
-		{
-			using (var db = new MyDbContext())
-			{
-				var files = from f in db.Object.Files
-							where folders.Contains(f.parent_folder)
-							select new FileToManipulate
-							{
-								file_id = f.file_id,
-								saved_path = f.saved_path
-							};
-				return files.ToList().Cast<AbstractFileToManipulate>().ToList();
-			}
-		}
-
+		
 		private void deleteLabelFiles(List<AbstractFileToManipulate> pendingDeleteFiles)
 		{
 			using (var conn = new SQLiteConnection(MyDbContext.ConnectionString))
@@ -254,7 +240,7 @@ namespace InfiniteStorage.REST
 						var temp_path = Path.Combine(recycleBinPath, Guid.NewGuid().ToString());
 
 						file.Move(temp_path);
-						file.recycle_bin_path = temp_path;
+						file.temp_file_path = temp_path;
 						pendingDeleteItems.Add(file);
 					}
 					else
@@ -270,144 +256,12 @@ namespace InfiniteStorage.REST
 			}
 			return pendingDeleteItems;
 		}
-
-		private string[] parseToList(string para)
-		{
-			if (string.IsNullOrEmpty(para))
-				return new string[] { };
-
-			return para.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-		}
-
-
-		private List<AbstractFileToManipulate> getFiles(List<Guid> file_ids)
-		{
-			var ret = new List<AbstractFileToManipulate>();
-
-			using (var conn = new SQLiteConnection(MyDbContext.ConnectionString))
-			{
-				conn.Open();
-				using (var cmd = conn.CreateCommand())
-				{
-					cmd.CommandText = "select 0 as kind, saved_path from files where file_id = @file and deleted = 0 " +
-									  "union " +
-									  "select 1 as kind, saved_path from pendingfiles where file_id = @file and deleted = 0";
-					cmd.Prepare();
-
-					foreach (var file_id in file_ids)
-					{
-						cmd.Parameters.Clear();
-						cmd.Parameters.Add(new SQLiteParameter("@file", file_id));
-						using (var reader = cmd.ExecuteReader())
-						{
-							if (reader.Read())
-							{
-								var saved_path = reader["saved_path"].ToString();
-								var kind = (long)reader["kind"];
-
-								if (kind == 0)
-									ret.Add(new FileToManipulate { file_id = file_id, saved_path = saved_path });
-								else
-									ret.Add(new PendingFileToManipulate { file_id = file_id, saved_path = saved_path });
-							}
-						}
-					}
-				}
-			}
-
-			return ret;
-		}
 	}
 
 
-	internal abstract class AbstractFileToManipulate
-	{
-		public Guid file_id { get; set; }
-		public string saved_path { get; set; }
-		public string recycle_bin_path { get; set; }
-
-		public abstract string saved_full_path { get; }
-
-		protected AbstractFileToManipulate()
-		{
-
-		}
-
-		public string small_thumb_path
-		{
-			get { return Path.Combine(MyFileFolder.Thumbs, file_id + ".small.thumb"); }
-		}
-
-		public string medium_thumb_path
-		{
-			get { return Path.Combine(MyFileFolder.Thumbs, file_id + ".small.thumb"); }
-		}
-
-		public string large_thumb_path
-		{
-			get { return Path.Combine(MyFileFolder.Thumbs, file_id + ".small.thumb"); }
-		}
-
-		public string tiny_thumb_path
-		{
-			get { return Path.Combine(MyFileFolder.Thumbs, file_id + ".small.thumb"); }
-		}
-
-		public void DeleteThumbnails()
-		{
-			var small = new FileInfo(small_thumb_path);
-			delete(small);
-
-			var medium = new FileInfo(medium_thumb_path);
-			delete(medium);
-
-			var large = new FileInfo(large_thumb_path);
-			delete(large);
-
-			var tiny = new FileInfo(tiny_thumb_path);
-			delete(tiny);
-		}
-
-		public void DeleteRecycleBinFile()
-		{
-			if (!string.IsNullOrEmpty(recycle_bin_path))
-				delete(new FileInfo(recycle_bin_path));
-		}
-
-		private void delete(FileInfo file)
-		{
-			try
-			{
-				if (file.Exists)
-					file.Delete();
-			}
-			catch (Exception err)
-			{
-				log4net.LogManager.GetLogger(GetType()).Warn("Unable to delete file: " + file.FullName, err);
-			}
-		}
-
-		public void Move(string dest)
-		{
-			File.Move(saved_full_path, dest);
-		}
-	}
+	
 
 
-	internal class FileToManipulate : AbstractFileToManipulate
-	{
-		public override string saved_full_path
-		{
-			get { return Path.Combine(MyFileFolder.Photo, saved_path); }
-		}
-	}
-
-	internal class PendingFileToManipulate: AbstractFileToManipulate
-	{
-		public override string saved_full_path
-		{
-			get { return Path.Combine(MyFileFolder.Pending, saved_path); }
-		}
-	}
+	
 
 }
---
+++ Sources/InfiniteStorage/Src/Class/REST/ManipulationMoveApiHandler.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wammer.Station;
+using System.IO;
+using InfiniteStorage.Model;
+using System.Data.SQLite;
+
+namespace InfiniteStorage.REST
+{
+	class ManipulationMoveApiHandler : ManipulationApiHandlerBase
+	{
+		FileMover fileMover = new FileMover();
+
+		public override void HandleRequest()
+		{
+			CheckParameter("targetPath");
+
+			var file_ids = GetIds();
+			var folders = GetPaths();
+			var full_target_path = Parameters["targetPath"];
+
+			if (!full_target_path.StartsWith(MyFileFolder.Photo))
+				throw new ArgumentException("invalid targetPath");
+
+
+			var partial_taget_path = PathUtil.MakeRelative(full_target_path, MyFileFolder.Photo);
+
+			var filesToMove = GetFilesById(file_ids);
+			filesToMove.AddRange(GetFilesFromFolder(folders));
+
+			if (!Directory.Exists(full_target_path))
+			{
+				Directory.CreateDirectory(full_target_path);
+				addFolderRecord(partial_taget_path);
+			}
+
+
+			List<AbstractFileToManipulate> movedFiles;
+			List<AbstractFileToManipulate> notMovedFiles;
+			moveFiles(full_target_path, filesToMove, out movedFiles, out notMovedFiles);
+
+			updateFileRecords(movedFiles, partial_taget_path);
+
+
+			respondSuccess(new {
+				api_ret_code = 0,
+				api_ret_message = "success",
+				status = 200,
+
+				moved_files = movedFiles.Select(x=>x.file_id),
+				not_moved_files = notMovedFiles.Select(x=>x.file_id)
+			});
+
+		}
+
+		private void updateFileRecords(List<AbstractFileToManipulate> movedFiles, string partial_folder_path)
+		{
+			using (var conn = new SQLiteConnection(MyDbContext.ConnectionString))
+			{
+				conn.Open();
+				using (var transaction = conn.BeginTransaction())
+				using (var cmdUpdate = conn.CreateCommand())
+				using (var cmdInsert = conn.CreateCommand())
+				{
+					cmdUpdate.CommandText = "update Files set saved_path = @saved, parent_folder = @parent where file_id = @file";
+					cmdUpdate.Prepare();
+
+					cmdInsert.CommandText =
+						"insert into Files (file_id, file_name, file_path, file_size, saved_path, parent_folder, device_id, type, event_time, seq, deleted, thumb_ready, width, height, orientation) " +
+						"select file_id, file_name, file_path, file_size, @saved, @parent, device_id, type, event_time, @seq, deleted, thumb_ready, width, height, orientation from [PendingFiles] " +
+						"where file_id = @fid; " +
+						
+						"delete from pendingFiles where file_id = @fid";
+					cmdInsert.Prepare();
+
+					foreach (var file in movedFiles)
+					{
+						if (file.IsPendingFile)
+						{
+							cmdInsert.Parameters.Clear();
+							cmdInsert.Parameters.Add(new SQLiteParameter("@saved", PathUtil.MakeRelative(file.temp_file_path, MyFileFolder.Photo)));
+							cmdInsert.Parameters.Add(new SQLiteParameter("@parent", partial_folder_path));
+							cmdInsert.Parameters.Add(new SQLiteParameter("@fid", file.file_id));
+							cmdInsert.Parameters.Add(new SQLiteParameter("@seq", SeqNum.GetNextSeq()));
+							cmdInsert.ExecuteNonQuery();
+						}
+						else
+						{
+							cmdUpdate.Parameters.Clear();
+							cmdUpdate.Parameters.Add(new SQLiteParameter("@saved", PathUtil.MakeRelative(file.temp_file_path, MyFileFolder.Photo)));
+							cmdUpdate.Parameters.Add(new SQLiteParameter("@parent", partial_folder_path));
+							cmdUpdate.Parameters.Add(new SQLiteParameter("@file", file.file_id));
+							cmdUpdate.ExecuteNonQuery();
+						}
+					}
+
+					transaction.Commit();
+				}
+			}
+		}
+
+		private void moveFiles(string targetPath, List<AbstractFileToManipulate> filesToMove, out List<AbstractFileToManipulate> movedFiles, out List<AbstractFileToManipulate> notMovedFiles)
+		{
+			movedFiles = new List<AbstractFileToManipulate>();
+			notMovedFiles = new List<AbstractFileToManipulate>();
+
+			foreach (var file in filesToMove)
+			{
+				try
+				{
+					var newPath = Path.Combine(targetPath, file.file_name);
+					
+					file.temp_file_path = fileMover.Move(file.saved_full_path, newPath);
+
+					movedFiles.Add(file);
+				}
+				catch (Exception err)
+				{
+					log4net.LogManager.GetLogger(GetType()).Warn("Unable to move file: " + file.saved_full_path, err);
+					notMovedFiles.Add(file);
+				}
+			}
+		}
+
+		private void addFolderRecord(string targetPath)
+		{
+			using (var db = new MyDbContext())
+			{
+				var q = from folder in db.Object.Folders
+						where folder.path == targetPath
+						select folder;
+
+				if (q.FirstOrDefault() != null)
+					return;
+
+				db.Object.Folders.Add(new Folder { path = targetPath, parent_folder = Path.GetDirectoryName(targetPath), name = Path.GetFileName(targetPath) });
+				db.Object.SaveChanges();
+			}
+		}
+
+		private string createTempFolder()
+		{
+			string tempPath;
+
+			do
+			{
+				tempPath = Path.Combine(MyFileFolder.Photo, "." + Guid.NewGuid().ToString());
+
+			} while (Directory.Exists(tempPath));
+
+
+			var dir = new DirectoryInfo(tempPath);
+			dir.Create();
+			dir.Attributes |= FileAttributes.Hidden;
+
+			return tempPath;
+		}
+	}
+}
