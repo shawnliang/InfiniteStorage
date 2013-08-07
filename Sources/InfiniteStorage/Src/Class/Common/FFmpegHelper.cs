21eed98648e3227c6d64072f8a159d3f4ef5632f
Shawn Liang <shawn.liang@waveface.com>
2013-08-07 12:30:11 +0800

    fix: uploaded mp4 videos have no sound

--- Sources/InfiniteStorage/Src/Class/Common/FFmpegHelper.cs
+++ Sources/InfiniteStorage/Src/Class/Common/FFmpegHelper.cs
@@ -20,14 +20,14 @@ namespace InfiniteStorage.Common
 
 		public static void MakeVideoThumbnail(string video_path, string thumb_path)
 		{
-			var args = string.Format("-i \"{0}\" -an -vcodec libx264 -crf 23 \"{1}\"", video_path, thumb_path);
+			var args = string.Format("-i \"{0}\" -vcodec libx264 -r 24 -acodec copy \"{1}\"", video_path, thumb_path);
 
 			makeVideoThumbnail(thumb_path, args);
 		}
 
 		public static void MakeVideoThumbnail720(string video_path, string thumb_path)
 		{
-			var args = string.Format("-i \"{0}\" -an -vcodec libx264 -crf 23 -vf \"scale=720:trunc(ow/a/2)*2\" \"{1}\"", video_path, thumb_path);
+			var args = string.Format("-i \"{0}\" -vcodec libx264 -r 24 -vf \"scale=720:trunc(ow/a/2)*2\" \"{1}\"", video_path, thumb_path);
 
 			makeVideoThumbnail(thumb_path, args);
 		}
