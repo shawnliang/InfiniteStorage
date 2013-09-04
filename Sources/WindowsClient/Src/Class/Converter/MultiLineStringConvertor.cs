cdc9958eaac69ea4390339ece18c4396a29dd88a
larrynung <larrynung@hotmail.com>
2013-09-04 11:17:08 +0800

    adjust string table

---
+++ Sources/WindowsClient/Src/Class/Converter/MultiLineStringConvertor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Waveface.Client
+{
+	public class MultiLineStringConvertor : IValueConverter
+	{
+		public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			var str = value as String;
+			
+			if (str == null)
+				return null;
+
+			return str.Replace("\\r\\n","\r\n");
+		}
+
+		public Object ConvertBack(Object value, Type targetType,
+			Object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
