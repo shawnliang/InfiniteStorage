ac6542c11f248c3474f3e2fde5304b9642c7c954
larrynung <larrynung@hotmail.com>
2013-09-04 15:56:00 +0800

    implement content area's drag selection mode

--- Sources/WindowsClient/Src/Window/MainWindow.xaml.cs
+++ Sources/WindowsClient/Src/Window/MainWindow.xaml.cs
@@ -1331,6 +1331,9 @@ namespace Waveface.Client
 
 		private void List_MouseMove(object sender, MouseEventArgs e)
 		{
+            if (isDraggingSelectionRect)
+                return;
+
 			// Get the current mouse position
 			Point mousePos = e.GetPosition(null);
 			Vector diff = startPoint - mousePos;
@@ -1662,11 +1665,6 @@ namespace Waveface.Client
 			}
 		}
 
-		private void lbxContentContainer_MouseDown(object sender, MouseButtonEventArgs e)
-		{
-			lbxContentContainer.UnselectAll();
-		}
-
 		private void GettingStarted_Tip1Clicked(object sender, MouseButtonEventArgs e)
 		{
 			showWaitForPairingDialog();
@@ -1844,5 +1842,208 @@ namespace Waveface.Client
 
 			GC.Collect();
 		}
+
+
+        /// <summary>
+        /// Set to 'true' when the left mouse-button is down.
+        /// </summary>
+        private bool isLeftMouseButtonDownOnWindow = false;
+
+        /// <summary>
+        /// Set to 'true' when dragging the 'selection rectangle'.
+        /// Dragging of the selection rectangle only starts when the left mouse-button is held down and the mouse-cursor
+        /// is moved more than a threshold distance.
+        /// </summary>
+        private bool isDraggingSelectionRect = false;
+
+        /// <summary>
+        /// Records the location of the mouse (relative to the window) when the left-mouse button has pressed down.
+        /// </summary>
+        private Point origMouseDownPoint;
+
+        /// <summary>
+        /// The threshold distance the mouse-cursor must move before drag-selection begins.
+        /// </summary>
+        private static readonly double DragThreshold = 5;
+
+
+        /// <summary>
+        /// Event raised when the user presses down the left mouse-button.
+        /// </summary>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isLeftMouseButtonDownOnWindow = true;
+                origMouseDownPoint = e.GetPosition(lbxContentContainer);
+
+                //this.CaptureMouse();
+
+                //e.Handled = true;
+            }
+        }
+
+
+        /// <summary>
+        /// Event raised when the user releases the left mouse-button.
+        /// </summary>
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (isDraggingSelectionRect)
+                {
+                    //
+                    // Drag selection has ended, apply the 'selection rectangle'.
+                    //
+
+                    isDraggingSelectionRect = false;
+                    ApplyDragSelectionRect();
+
+                    e.Handled = true;
+                }
+
+                if (isLeftMouseButtonDownOnWindow)
+                {
+                    isLeftMouseButtonDownOnWindow = false;
+                    //this.ReleaseMouseCapture();
+
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingSelectionRect)
+            {
+                //
+                // Drag selection is in progress.
+                //
+                Point curMouseDownPoint = e.GetPosition(lbxContentContainer);
+                UpdateDragSelectionRect(origMouseDownPoint, curMouseDownPoint);
+
+                e.Handled = true;
+            }
+            else if (isLeftMouseButtonDownOnWindow)
+            {
+                //
+                // The user is left-dragging the mouse,
+                // but don't initiate drag selection until
+                // they have dragged past the threshold value.
+                //
+                Point curMouseDownPoint = e.GetPosition(lbxContentContainer);
+                var dragDelta = curMouseDownPoint - origMouseDownPoint;
+                double dragDistance = Math.Abs(dragDelta.Length);
+                if (dragDistance > DragThreshold)
+                {
+                    //
+                    // When the mouse has been dragged more than the threshold value commence drag selection.
+                    //
+                    isDraggingSelectionRect = true;
+
+                    //
+                    //  Clear selection immediately when starting drag selection.
+                    //
+                    lbxContentContainer.SelectedItems.Clear();
+
+                    InitDragSelectionRect(origMouseDownPoint, curMouseDownPoint);
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Initialize the rectangle used for drag selection.
+        /// </summary>
+        private void InitDragSelectionRect(Point pt1, Point pt2)
+        {
+            UpdateDragSelectionRect(pt1, pt2);
+
+            dragSelectionCanvas.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Update the position and size of the rectangle used for drag selection.
+        /// </summary>
+        private void UpdateDragSelectionRect(Point pt1, Point pt2)
+        {
+            double x, y, width, height;
+
+            //
+            // Determine x,y,width and height of the rect inverting the points if necessary.
+            // 
+
+            if (pt2.X < pt1.X)
+            {
+                x = pt2.X;
+                width = pt1.X - pt2.X;
+            }
+            else
+            {
+                x = pt1.X;
+                width = pt2.X - pt1.X;
+            }
+
+            if (pt2.Y < pt1.Y)
+            {
+                y = pt2.Y;
+                height = pt1.Y - pt2.Y;
+            }
+            else
+            {
+                y = pt1.Y;
+                height = pt2.Y - pt1.Y;
+            }
+
+            //
+            // Update the coordinates of the rectangle used for drag selection.
+            //
+            Canvas.SetLeft(dragSelectionBorder, x);
+            Canvas.SetTop(dragSelectionBorder, y);
+            dragSelectionBorder.Width = width;
+            dragSelectionBorder.Height = height;
+        }
+
+        /// <summary>
+        /// Select all nodes that are in the drag selection rectangle.
+        /// </summary>
+        private void ApplyDragSelectionRect()
+        {
+            dragSelectionCanvas.Visibility = Visibility.Collapsed;
+
+            double x = Canvas.GetLeft(dragSelectionBorder);
+            double y = Canvas.GetTop(dragSelectionBorder);
+            double width = dragSelectionBorder.Width;
+            double height = dragSelectionBorder.Height;
+            Rect dragRect = new Rect(x, y, width, height);
+
+            //
+            // Inflate the drag selection-rectangle by 1/10 of its size to 
+            // make sure the intended item is selected.
+            //
+            dragRect.Inflate(width / 10, height / 10);
+
+            //
+            // Clear the current selection.
+            //
+            lbxContentContainer.SelectedItems.Clear();
+
+            //
+            // Find and select all the list box items.
+            //
+            foreach (var item in lbxContentContainer.Items)
+            {
+                var itemContainer = lbxContentContainer.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+                var itemTransform = itemContainer.TransformToAncestor(svContentContainer);
+                var itemRectangle = itemTransform.TransformBounds(new Rect(new Point(0, 0), itemContainer.RenderSize));
+
+                if (dragRect.Contains(itemRectangle))
+                {
+                    lbxContentContainer.SelectedItems.Add(item);
+                }
+            }
+        }
 	}
 }
