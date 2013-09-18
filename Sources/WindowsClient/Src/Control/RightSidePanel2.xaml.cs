932f3090a2cc1ef187ab266c2001f1161ed429ab
Shawn Liang <shawn.liang@waveface.com>
2013-09-18 12:05:35 +0800

    add zh-CN to installer

--- Sources/WindowsClient/Src/Control/RightSidePanel2.xaml.cs
+++ Sources/WindowsClient/Src/Control/RightSidePanel2.xaml.cs
@@ -162,35 +162,5 @@ namespace Waveface.Client
 			tbLinkOpenClose.Text = FindResource("RightSide_CloudSharing_StatusClosed") as string;
 			tipText.Visibility = Visibility.Visible;
 		}
-
-		//private void tbxName_LostFocus(object sender, RoutedEventArgs e)
-		//{
-		//	if (m_oldName != tbxName.Text)
-		//	{
-		//		string _name = tbxName.Text;
-
-		//		while (IsNewFavoriteNameExist(_name))
-		//		{
-		//			_name += " (1)";
-		//		}
-
-		//		StationAPI.RenameLabel(m_oldFavoriteID, _name);
-
-		//		tbxName.Text = _name;
-		//	}
-		//}
-
-		//private bool IsNewFavoriteNameExist(string name)
-		//{
-		//	foreach (IContentGroup _group in ClientFramework.Client.Default.GetFavorites(true))
-		//	{
-		//		if (_group.Name == name)
-		//		{
-		//			return true;
-		//		}
-		//	}
-
-		//	return false;
-		//}
 	}
 }
